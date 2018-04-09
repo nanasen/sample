e4b945bf82e6c8722dbd3f6d7a881d758d3d5a9f
sonofbeach <908375941@qq.com>
2018-04-09 02:41:24 +0000

    Finish user signup

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('/create', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
