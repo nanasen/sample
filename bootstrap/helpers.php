0478aff26817e658840b086a9e6ce66a8dcbb7f4
sonofbeach <908375941@qq.com>
2018-04-09 03:07:38 +0000

    fix the helpers.php->'IS_IN_HEROKU'

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,6 +1,6 @@
 <?php
  function get_db_config(){
-    if(getenv(IS_IN_HEROKU)){
+    if(getenv('IS_IN_HEROKU')){
         $url = parse_url(getenv("DATABASE_URL"));
         return $db_config = [
             'connerction' => 'pgsql',
