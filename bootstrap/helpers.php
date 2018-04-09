1d9724c3d3ef6f8dc484ae89ca28f22d9c600485
sonofbeach <908375941@qq.com>
2018-04-09 04:14:13 +0000

    change database configuration

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,21 +1,23 @@
 <?php
- function get_db_config(){
-    if(getenv('IS_IN_HEROKU')){
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
         $url = parse_url(getenv("DATABASE_URL"));
+
         return $db_config = [
-            'connerction' => 'pgsql',
-            'host' => $url['host'],
-            'database' => substr($url["path"], 1),
-            'username' => $url["user"],
-            'password' => $url["pass"],
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
         ];
-    }else{
+    } else {
         return $db_config = [
-            'connerction' => env('DB_CONNECTION', 'mysql'),
+            'connection' => env('DB_CONNECTION', 'mysql'),
             'host' => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env("DB_PASSWORD", ''),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
         ];
     }
- }
+}
