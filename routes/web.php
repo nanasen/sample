e2521f91d580cdc73a724b0650a8749e0f990f0b
sonofbeach <908375941@qq.com>
2018-04-22 02:10:42 +0000

    Finish login and logout

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('/create', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('loout', 'SessionsController@destroy')->name('logout');
